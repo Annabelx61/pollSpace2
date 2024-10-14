d8f2f9b1fbdb806e0ec92dd417a7d4515e28a689
Sarah <grahamsl@hendrix.edu>
2024-10-14 12:03:06 -0500

    adding more polls

--- lib/polls.dart
+++ lib/polls.dart
@@ -1,109 +1,110 @@
-
 List polls = [
   {
     'id': 1,
-    'question':
-        'Which season is the best?',
+    'question': 'Which season is the best?',
     'end_date': DateTime(2025, 11, 21),
     'options': [
       {
         'id': 1,
         'title': 'Winter',
-        'image':'lib/assets/seasons/winter.jpg',
+        'image': 'lib/assets/seasons/winter.jpg',
         'votes': 40,
       },
       {
         'id': 2,
         'title': 'Autumn',
-        'image':'lib/assets/seasons/autumn.jpeg',
+        'image': 'lib/assets/seasons/autumn.jpeg',
         'votes': 20,
       },
       {
         'id': 3,
         'title': 'Summer',
-        'image':'lib/assets/seasons/summer.jpg',
+        'image': 'lib/assets/seasons/summer.jpg',
         'votes': 10,
       },
       {
         'id': 3,
         'title': 'Fall',
-        'image':'lib/assets/seasons/fall.jpg',
+        'image': 'lib/assets/seasons/fall.jpg',
         'votes': 10,
       },
     ],
   },
-//   {
-//     'id': 2,
-//     'question': 'Do you think Oranguntans have the ability speak?',
-//     'end_date': DateTime(2022, 12, 25),
-//     'options': [
-//       {
-//         'id': 1,
-//         'title': 'Yes, they definitely do',
-//         'votes': 40,
-//       },
-//       {
-//         'id': 2,
-//         'title': 'No, they do not',
-//         'votes': 0,
-//       },
-//       {
-//         'id': 3,
-//         'title': 'I do not know',
-//         'votes': 10,
-//       },
-//       {
-//         'id': 4,
-//         'title': 'Why should I care?',
-//         'votes': 30,
-//       }
-//     ],
-//   },
-//   {
-//     'id': 3,
-//     'question': 'What is the meaning of life?',
-//     'end_date': DateTime(2026, 09, 30),
-//     'options': [
-//       {
-//         'id': 1,
-//         'title': 'To love',
-//         'votes': 42,
-//       },
-//       {
-//         'id': 2,
-//         'title': 'To live',
-//         'votes': 10,
-//       },
-//       {
-//         'id': 3,
-//         'title': 'To die',
-//         'votes': 19,
-//       },
-//       {
-//         'id': 4,
-//         'title': 'To be happy',
-//         'votes': 25,
-//       },
-//     ],
-//   },
-//   {
-//     'id': 4,
-//     'question':
-//         'Which pet is better?',
-//     'end_date': DateTime(2025, 04, 30),
-//     'options': [
-//       {
-//         'id': 1,
-//         'title': 'Cats!',
-//         'votes': 1,
-//       },
-//       {
-//         'id': 2,
-//         'title': 'Obviously, dogs!',
-//         'votes': 0,
-//       },
-//     ],
-//     'hasVoted': true,
-//     'userVotedOptionId': 1,
-//   }
+  {
+    'id': 2,
+    'question': 'What superpower is the best?',
+    'end_date': DateTime(2024, 12, 25),
+    'options': [
+      {
+        'id': 1,
+        'title': 'Flying',
+        'image': 'lib/assets/superpower/flying.jpg',
+        'votes': 40,
+      },
+      {
+        'id': 2,
+        'title': 'Super strength',
+        'image': 'lib/assets/superpower/strength.jpg',
+        'votes': 0,
+      },
+      {
+        'id': 3,
+        'title': 'Invisibility',
+        'image': 'lib/assets/superpower/invisible.jpg',
+        'votes': 10,
+      },
+      {
+        'id': 4,
+        'title': 'Teleportation',
+        'image': 'lib/assets/superpower/teleport.jpg',
+        'votes': 30,
+      }
+    ],
+  },
+  {
+    'id': 3,
+    'question': 'Who would win in a fight?',
+    'end_date': DateTime(2026, 09, 30),
+    'options': [
+      {
+        'id': 1,
+        'title': 'Gorilla',
+        'image': 'lib/assets/gorilla_vs_bear/gorilla.jpg',
+        'votes': 42,
+      },
+      {
+        'id': 2,
+        'title': 'Bear',
+        'image': 'lib/assets/gorilla_vs_bear/bear.jpg',
+        'votes': 10,
+      }
+    ],
+    // },
+    // {
+    //     'id': 4,
+    //     'question':'What's the best name for a pet rock?',
+    //     'end_date': DateTime(2025, 04, 30),
+    //     'options': [
+    //       {
+    //        'id': 1,
+    //       'title': 'Dwayne "The Rock"!',
+    //         'image': 'lib/assets/rock/therock.jpg',
+    //        'votes': 1,
+    //      },
+    //      {
+    //        'id': 2,
+    //        'title': 'Pebbles',
+    //        'image': 'lib/assets/rock/rock1.jpg',
+    //        'votes': 0,
+    //      },
+    //       {
+    //        'id': 3,
+    //        'title': 'Rolling Stone',
+    //         'image': 'lib/assets/rock/rock2.jpg',
+    //        'votes': 0,
+    //      },
+    //    ],
+    //    'hasVoted': true,
+    //    'userVotedOptionId': 1,
+  }
 ];
