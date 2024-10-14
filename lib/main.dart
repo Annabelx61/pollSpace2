eb49ad8ff1d5c6d7a58cff919183c42b559bd44f
Annabelx61 <MehariAA@hendrix.edu>
2024-10-14 12:55:00 -0500

    Fixed main, 1 issue left

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter_application_1/firebase_options.dart';
+import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-import 'home_page.dart';
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
