057d079c35dff3bd6dc0345b9afb08ec642be25c
Riley Skief <skiefri@hendrix.edu>
2024-11-21 22:03:09 -0600

    finished profile pic intake page

--- lib/profile_pic.dart
+++ lib/profile_pic.dart
@@ -1,8 +1,26 @@
+import 'dart:typed_data';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-class ProfilePicture extends StatelessWidget {
+// help from https://www.youtube.com/watch?v=tCN395wN3pY 
+
+class ProfilePicture extends StatefulWidget {
   const ProfilePicture({super.key});
+
+  @override
+  State<ProfilePicture> createState() => _ProfilePictureState();
+}
+
+class _ProfilePictureState extends State<ProfilePicture> {
+  Uint8List? pickedImage;
+
+  @override
+  void initState() {
+    super.initState();
+    getProfilePicture();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +30,15 @@ class ProfilePicture extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.grey[400],
         shape: BoxShape.circle,
+        image: pickedImage != null
+          ? DecorationImage(
+            fit: BoxFit.cover,
+            image: Image.memory(
+              pickedImage!,
+              fit: BoxFit.cover,
+            ).image,
+          )
+        : null,
       ),
       child: const Center(
         child: Icon(
@@ -28,7 +55,24 @@ class ProfilePicture extends StatelessWidget {
 
     final storageRef = FirebaseStorage.instance.ref();
     final imageRef = storageRef.child("user_1.jpg");
+    // 'user_1'needs to be whatever the user id is
+    final imageBytes = await image.readAsBytes();
+    await imageRef.putData(imageBytes);
+
+    setState(() => pickedImage = imageBytes);
   }
 
+  Future<void> getProfilePicture() async {
+    final storageRef = FirebaseStorage.instance.ref();
+    final imageRef = storageRef.child('user_1.jpg');
+
+    try {
+      final imageBytes = await imageRef.getData();
+      if (imageBytes == null) return;
+      setState(() => pickedImage = imageBytes);
+    } catch (e) {
+      print('Profile picture could not be found');
+    }
+  }
 
 }
